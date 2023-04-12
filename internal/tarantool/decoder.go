f159ad4437a0322e0038f61fea90ea0c25f18324
Peter <isupov@biocad.ru>
2023-04-12 12:30:36 +0300

    add support repeated enum

--- internal/tarantool/decoder.go
+++ internal/tarantool/decoder.go
@@ -48,7 +48,7 @@ func decode(g *protogen.GeneratedFile, msg *protogen.Message) {
 		if f.Enum != nil {
 			// если enum и slice, то декодер по типу enum
 			if f.Desc.Cardinality().String() == "repeated" {
-				cst := decodeGen(f.Enum.GoIdent.String())
+				cst := decodeGen(f.Enum.GoIdent.GoName)
 				cst.slcDecodeMessage(g, f.GoName)
 
 				continue
